e3a525bbeb79f99978b9e5e3f2877f294f629f36
Camden <naviscam@gmail.com>
2018-12-13 15:36:27 -0500

    Updates

---
+++ precache-manifest.1a4cdeb253bcb698545cc233f1a4e187.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "067a08fecf89eef6373d",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "067a08fecf89eef6373d",
+    "url": "/contactmanager/static/js/main.067a08fe.chunk.js"
+  },
+  {
+    "revision": "4919e28c9b2b0f5630de",
+    "url": "/contactmanager/static/css/1.ecf8196c.chunk.css"
+  },
+  {
+    "revision": "4919e28c9b2b0f5630de",
+    "url": "/contactmanager/static/js/1.4919e28c.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "155fb94057f1a6510e4ed590b8554f8b",
+    "url": "/contactmanager/index.html"
+  }
+];
